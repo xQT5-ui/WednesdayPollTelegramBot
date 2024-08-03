e785d0485d0524616140678432723263d1050f58
Antony Grachev <personalsmile69@gmail.com>
2024-08-03 19:53:24 +0300

    add (un)pin poll

--- app/lib/TelegramBot.go
+++ app/lib/TelegramBot.go
@@ -14,26 +14,48 @@ func CreateBot(chat_id int, fact, question string, answersYes, answersNo []strin
 	// Get bot token
 	bot_token := DecryptBotToken(config.Bot_secure.Bot_token, config.Bot_secure.Bot_hash, log)
 
-	bot, err := tb.NewBot(tb.Settings{
+	// Set preferences for bot
+	pref := tb.Settings{
 		Token: bot_token,
-		// Time for bot reading messages
 		Poller: &tb.LongPoller{
+			// Time for bot reading messages
 			Timeout:        10 * time.Second,
 			AllowedUpdates: []string{"message", "edited_message", "channel_post", "edited_channel_post"}},
-	})
+	}
+
+	// Create bot
+	bot, err := tb.NewBot(pref)
 	if err != nil {
 		log.Fatal(err, "Ошибка создания бота:")
 	}
 
 	startReadingTime := time.Now()
+	var msg *tb.Message
 
 	// Add command's handlers for debug
 	bot.Handle("/sendpoll", func(c tb.Context) error {
 		// Check all message in queue, their time and if it is more than now then run logic!
 		if c.Message().Time().After(startReadingTime) {
-			SendPoll(bot, chat_id, fact, question, answersYes, answersNo, log)
+			msg = sendPoll(bot, c.Chat(), fact, question, answersYes, answersNo, log)
+			// Save poll msg ID
+			// c.Set("poll_msg_id", msg.ID)
 		}
+		return nil
+	})
 
+	bot.Handle("/pinpoll", func(c tb.Context) error {
+		// Check all message in queue, their time and if it is more than now then run logic!
+		if c.Message().Time().After(startReadingTime) {
+			pinMsg(bot, c.Chat(), log, msg)
+		}
+		return nil
+	})
+
+	bot.Handle("/unpinpoll", func(c tb.Context) error {
+		// Check all message in queue, their time and if it is more than now then run logic!
+		if c.Message().Time().After(startReadingTime) {
+			unpinMsg(bot, c.Chat(), log)
+		}
 		return nil
 	})
 
@@ -42,16 +64,18 @@ func CreateBot(chat_id int, fact, question string, answersYes, answersNo []strin
 	return bot
 }
 
-func SendPoll(bot *tb.Bot, chat_id int, fact, question string, answersYes, answersNo []string, log *lg.Logger) {
+func sendPoll(bot *tb.Bot, chat *tb.Chat, fact, question string, answersYes, answersNo []string, log *lg.Logger) *tb.Message {
 	if question == "" {
 		log.Fatal(fmt.Errorf("отсутствует вопрос. Заполните его в конфигурационном файле"), "")
 	}
+
 	result_message := fmt.Sprintf("%s\n%s", fact, question)
 
 	// Get answers
 	if len(answersYes) == 0 || len(answersNo) == 0 {
 		log.Fatal(fmt.Errorf("отсутствуют варианты ответов. Заполните их в конфигурационном файле"), "")
 	}
+	rand.Seed(34)
 	rnd_num := rand.Intn(len(answersYes))
 	answers := []string{answersYes[rnd_num], answersNo[rnd_num]}
 	// Set answer's options for poll
@@ -60,24 +84,71 @@ func SendPoll(bot *tb.Bot, chat_id int, fact, question string, answersYes, answe
 		{Text: answers[1], VoterCount: 0},
 	}
 
-	// Send poll
-	poll_message, err := bot.Send(
-		// Send to what's chat
-		&tb.Chat{ID: int64(chat_id)},
-		// Send poll message
-		&tb.Poll{
-			Question:  result_message,
-			Options:   poll_options,
-			Type:      tb.PollRegular,
-			Anonymous: false,
-		},
-	)
-	if err != nil {
-		log.Fatal(err, "Ошибка отправки опроса:\n%v")
-		// return nil
+	// Create poll message
+	poll_msg := &tb.Poll{
+		Question:  result_message,
+		Options:   poll_options,
+		Type:      tb.PollRegular,
+		Anonymous: false,
 	}
 
-	log.Info(fmt.Sprintf("Опрос успешно отправлен:\n%v", poll_message))
+	// Send poll
+	poll_message, err := bot.Send(
+		// Send's what's chat
+		// &tb.Chat{ID: int64(chat_id)},
+		chat,
+		// Send's poll message
+		poll_msg,
+	)
+	if err != nil {
+		log.Fatal(err, "Ошибка отправки опроса:")
+		return nil
+	}
 
-	// return poll_message
+	log.Info(fmt.Sprintf("Опрос успешно отправлен:\n%v", poll_message.ID))
+
+	return poll_message
+}
+
+func pinMsg(bot *tb.Bot, c *tb.Chat, log *lg.Logger, new_poll *tb.Message) {
+	chat, err := bot.ChatByID(c.ID)
+	if err != nil {
+		log.Fatal(err, "Ошибка получения чата:")
+		return
+	}
+
+	// Get pin messages
+	pinnedMessage := chat.PinnedMessage
+
+	if pinnedMessage != nil && pinnedMessage.Poll != nil && pinnedMessage.ID == new_poll.ID {
+		log.Info(fmt.Sprintf("Опрос уже закреплен с ID = '%v'", new_poll.ID))
+		return
+	} else {
+		// Pin poll
+		err := bot.Pin(new_poll, tb.Silent)
+		if err != nil {
+			log.Fatal(err, fmt.Sprintf("Ошибка закрепления опроса с ID = '%v'", new_poll.ID))
+		}
+		log.Info(fmt.Sprintf("Опрос закреплен ID = '%v'", new_poll.ID))
+	}
+}
+
+func unpinMsg(bot *tb.Bot, c *tb.Chat, log *lg.Logger) {
+	chat, err := bot.ChatByID(c.ID)
+	if err != nil {
+		log.Fatal(err, "Ошибка получения чата:")
+		return
+	}
+
+	// Get pin messages
+	pinnedMessage := chat.PinnedMessage
+
+	if pinnedMessage != nil && pinnedMessage.Poll != nil {
+		// Unpin poll
+		err := bot.Unpin(chat, pinnedMessage.ID)
+		if err != nil {
+			log.Fatal(err, "Ошибка открепления опроса:")
+		}
+		log.Info(fmt.Sprintf("Опрос откреплен c ID = '%v'", pinnedMessage.ID))
+	}
 }
